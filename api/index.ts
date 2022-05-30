10619527e3f88ae03391c5ff356961c654a795db
Renan Ferreira <relferreira@gmail.com>
2022-05-30 16:25:03 -0300

    Changing maxage to 24 hours

--- api/index.ts
+++ api/index.ts
@@ -1,26 +1,36 @@
-import { Request, Response } from '@vercel/node'
-import { getScreenshot } from './_lib/puppeteer';
+import { Request, Response } from "@vercel/node";
+import { getScreenshot } from "./_lib/puppeteer";
 
 module.exports = async (req: Request, res: Response) => {
-  const usage = "https://s.vercel.app/api?url=https://google.com&width=1280&height=720"
-  if (!req.query.url) return res.status(400).json({
-    "success": false,
-    "error": "No url query specified.",
-    "usage": usage
-  });
+  const usage =
+    "https://s.vercel.app/api?url=https://google.com&width=1280&height=720";
+  if (!req.query.url)
+    return res.status(400).json({
+      success: false,
+      error: "No url query specified.",
+      usage: usage,
+    });
   try {
-    const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
-    res.setHeader('Content-Type', `image/png`);
-    res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
+    const file = await getScreenshot(
+      req.query.url,
+      req.query.width,
+      req.query.height
+    );
+    res.setHeader("Content-Type", `image/png`);
+    res.setHeader(
+      "Cache-Control",
+      `public, immutable, no-transform, s-maxage=86400, max-age=86400`
+    );
     res.status(200).end(file);
   } catch (error) {
-    console.error(error)
-    res.setHeader('Content-Type', 'application/json');
+    console.error(error);
+    res.setHeader("Content-Type", "application/json");
     res.status(400).json({
-      "success": false,
-      "error": "The server encountered an error. You may have inputted an invalid query.",
+      success: false,
+      error:
+        "The server encountered an error. You may have inputted an invalid query.",
       //"dev": error,
-      "usage": usage
+      usage: usage,
     });
   }
-}
+};
